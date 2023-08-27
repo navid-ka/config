92dcd6c34c77de42711697e9827262a04d373d47
Bifrost <nkeyani-@student.42barcelona.com>
2023-08-27 11:19:23 +0200

    added fish configs

---
+++ .config/fish/config.fish
@@ -0,0 +1,9 @@
+set -g fish_greeting
+function fish_greeting
+    gum style \
+        --foreground 212 --border-foreground 212 --border normal \
+        --align center --width 70 --margin "0 2" --padding "2 4" \
+        'There is always one more bug to fix.'
+end
+alias vim="nvim"
+starship init fish | source
